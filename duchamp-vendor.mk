6374d949591039c27a7addfb7d73a39597d1350d
Ravinderrohilla123 <153418233+Ravinderrohilla123@users.noreply.github.com>
2024-02-20 17:41:41 +0530

    Update duchamp-vendor.mk

--- duchamp-vendor.mk
+++ duchamp-vendor.mk
@@ -2,8 +2,6 @@ PRODUCT_PACKAGES += \
     ImsService \
     mediatek-common \
     mediatek-framework \
-    mediatek-gwsd \
-    mediatek-gwsdv2 \
     mediatek-ims-base \
     mediatek-ims-common \
     mediatek-ims-extension-plugin \
