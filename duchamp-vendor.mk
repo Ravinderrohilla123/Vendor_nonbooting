705ad5868a37358a043e66ce9f3714792db3b3b4
Ravinderrohilla123 <153418233+Ravinderrohilla123@users.noreply.github.com>
2024-02-20 17:11:14 +0530

    Update duchamp-vendor.mk

--- duchamp-vendor.mk
+++ duchamp-vendor.mk
@@ -1,1 +1,12 @@
-include v
+PRODUCT_PACKAGES += \
+    ImsService \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-gwsd \
+    mediatek-gwsdv2 \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-ims-extension-plugin \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
